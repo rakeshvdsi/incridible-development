76e358a5d527cd72af80dd3e71fa932db4ee206a
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 00:23:35 +0530

    file modified

--- Sample.java
+++ Sample.java
@@ -1,9 +1,5 @@
 public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
-System.out.println("welcome from first java program");
-System.out.println("Git Stahing Practising");
- System.out.println("Java longuage is evergreen Technology");
-System.out.println("Java is ocean");
 }
 }
