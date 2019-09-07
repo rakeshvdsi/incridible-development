e425899479f4bee8c9809d83dcec3dd5dfbd50f6
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 00:23:35 +0530

    conflicts resolved

--- Sample.java
+++ Sample.java
@@ -4,6 +4,6 @@ System.out.println("welcome to GITLAB");
 System.out.println("welcome from first java program");
 System.out.println("Git Stahing Practising");
  System.out.println("Java longuage is evergreen Technology");
-
+System.out.println("Java is ocean");
 }
 }
