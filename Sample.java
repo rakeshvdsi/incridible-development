091770b74261690f66364c516a34c4ef284e86a0
rakeshvdsi <45713602+rakeshvdsi@users.noreply.github.com>
2019-09-08 00:08:00 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("welcome from first java program");
+System.out.println("Stahing");
 }
 }
