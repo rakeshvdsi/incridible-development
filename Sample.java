f46d8bf296f96c8d8375a10b1839bb205af98d7c
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 02:36:14 +0530

    cherry picking

--- Sample.java
+++ Sample.java
@@ -2,5 +2,7 @@ public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("Java Latest Version is 1.9");
+System.out.println("cherry");
+System.out.println("cherry picking practising");
 }
 }
