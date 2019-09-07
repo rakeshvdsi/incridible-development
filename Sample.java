d4e9452c1de2fce82703e92d655ea0cf329e1737
rakeshvdsi <45713602+rakeshvdsi@users.noreply.github.com>
2019-09-08 00:15:51 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,7 @@ public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("welcome from first java program");
 System.out.println("Git Stahing Practising");
+ System.out.println("Java longuage is evergreen Technology");
+
 }
 }
