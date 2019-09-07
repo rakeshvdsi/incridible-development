6ab3a438f41b7e8966231851258b978360273243
rakeshvdsi <45713602+rakeshvdsi@users.noreply.github.com>
2019-09-08 00:15:51 +0530

    file modified

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
-System.out.println("cherry picking practising");
+System.out.println("cherry");
 }
 }
