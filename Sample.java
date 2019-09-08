3cbec9ce71ca2b4e6ff953914ab133ab918c2a7b
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 13:40:58 +0530

    one line removed

--- Sample.java
+++ Sample.java
@@ -3,6 +3,5 @@ public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("Java Latest Version is 1.9");
 System.out.println("cherry");
-System.out.println("cherry picking practising");
 }
 }
