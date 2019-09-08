e010bb372e184c945fe4d032be9fe7f78e58fef4
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 13:20:27 +0530

    one line removed

--- Sample.java
+++ Sample.java
@@ -3,7 +3,5 @@ public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("cherry");
 System.out.println("cherry picking practising");
-System.out.println("ReactJS is Hot technology Now);
-  
 }
 }
