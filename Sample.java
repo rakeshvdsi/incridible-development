1c1be58377a5af67f421d783095583b29e3eb19e
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 13:40:58 +0530

    one line removed

--- Sample.java
+++ Sample.java
@@ -2,6 +2,5 @@ public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("cherry");
-System.out.println("cherry picking practising");
 }
 }
