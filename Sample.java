19f2a115924b9225fa61977296c0d72b56aba7fe
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 15:03:53 +0530

    one line added

--- Sample.java
+++ Sample.java
@@ -4,5 +4,6 @@ System.out.println("welcome to GITLAB");
 System.out.println("Java Latest Version is 1.9");
 System.out.println("Angular is Trending Client side Scripting Technology");
 System.out.println("ReactJS Hot Technology");
+System.out.println("ReactJS Bit Criticle  Technology");
 }
 }
