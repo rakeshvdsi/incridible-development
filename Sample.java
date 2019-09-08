3eefdff3b18e16c3f585aa40550f1802697f6d35
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 14:38:30 +0530

    one line added

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("Java Latest Version is 1.9");
+System.out.println("ReactJS Hot Technology");
 }
 }
