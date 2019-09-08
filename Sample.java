4b0ab0aa546017ad048a732fa6c8e07dcba47d26
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 12:42:41 +0530

    added new line

--- Sample.java
+++ Sample.java
@@ -1,7 +1,6 @@
 public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
-System.out.println("cherry");
-System.out.println("cherry picking practising");
+System.out.println("Java Latest Version is 1.9");
 }
 }
