82d7bbb1639ab3b5438d6fc9514dd6f85b906cc9
rakesh <rakesh.kvr3@gmail.com>
2019-09-08 14:38:30 +0530

    one line added

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ public class Sample{
 public static void main(String[] args){
 System.out.println("welcome to GITLAB");
 System.out.println("Java Latest Version is 1.9");
+System.out.println("Angular is little older than ReactJS");
 System.out.println("ReactJS Hot Technology");
 }
 }
